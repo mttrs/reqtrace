188c281ff22db464f4a5e8575b985a288c927ba7
Mitsuteru Sawa <mttrs@users.noreply.github.com>
2017-10-16 18:09:33 +0900

    Change to use httputil.DumpRequest

--- main.go
+++ main.go
@@ -1,63 +1,22 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"net/http"
+	"net/http/httputil"
 	"os"
-	"text/template"
 )
 
-type Content struct {
-	Body          string
-	ContentType   string
-	Host          string
-	Method        string
-	Referer       string
-	RemoteAddr    string
-	RequestURI    string
-	URL           string
-	UserAgent     string
-	XForwardedFor string
-}
-
-var content = `
-Remote address:  {{.RemoteAddr}}
-Request URI:     {{.RequestURI}}
-Referer:         {{.Referer}}
-User agent:      {{.UserAgent}}
-Host:            {{.Host}}
-Content-Type:    {{.ContentType}}
-Method:          {{.Method}}
-Body:            {{.Body}}
-X-Forwarded-For: {{.XForwardedFor}}
-`
-
 func handler(w http.ResponseWriter, r *http.Request) {
-	bufbody := new(bytes.Buffer)
-	bufbody.ReadFrom(r.Body)
-	body := bufbody.String()
-
-	trace := Content{
-		Body:          body,
-		ContentType:   r.Header.Get("Content-Type"),
-		Host:          r.Host,
-		Method:        r.Method,
-		Referer:       r.Referer(),
-		RemoteAddr:    r.RemoteAddr,
-		RequestURI:    r.RequestURI,
-		URL:           r.URL.Path,
-		UserAgent:     r.UserAgent(),
-		XForwardedFor: r.Header.Get("X-Forwarded-For"),
-	}
-	t := template.Must(template.New("content").Parse(content))
-	err := t.Execute(os.Stdout, trace)
+	// Take dump
+	dump, err := httputil.DumpRequest(r, true)
 	if err != nil {
-		fmt.Println("execute template:", err)
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
 	}
 
-	// Return nothing
-	fmt.Fprintf(w, "kk")
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, "kk!\n")
 }
 
 func main() {
