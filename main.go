68dbfc1aac36f2dd63f6ba16e9ac1beca5246935
Mitsuteru Sawa <mttrs@users.noreply.github.com>
2020-03-11 13:02:55 +0000

    Add cssHandler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
 	"os"
@@ -22,7 +23,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "go!\n")
 }
 
+func cssHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/css")
+	d, err := ioutil.ReadFile("assets/main.css")
+
+	if err != nil {
+		fmt.Fprintf(w, "No such file\n")
+	} else {
+		fmt.Fprintf(w, string(d))
+	}
+}
+
 func main() {
+	http.HandleFunc("/css", cssHandler)
 	http.HandleFunc("/", handler)
 
 	port := os.Getenv("PORT")
